4d7b55b72fe0604f84810bdfc447afff4e763f5a
Spandan <royspandan1@gmail.com>
2023-11-13 20:36:40 +0530

    Use popen instead of exec and fork

--- src/trace.c
+++ src/trace.c
@@ -18,50 +18,34 @@ void print_trace(char* prg_name) {
     printf("Stack trace:\n");
     for (int i = 1; i < size; i++) {
         // Execute addr2line and get prettified names
-        int pipefd[2];
-        pid_t cpid;
-        pipe(pipefd);
-        cpid = fork();
-        if (cpid == 0) {
-            // Child
-            close(pipefd[0]);
-            dup2(pipefd[WRITE], STDOUT_FILENO);
-            dup2(pipefd[WRITE], STDERR_FILENO);
-            char addr2line_cmd[512];
-            sprintf(addr2line_cmd, "addr2line -p -f -e %s %p", prg_name, buffer[i] - 1);
-            system(addr2line_cmd);
-            exit(EXIT_SUCCESS);
-        } else {
-            // Parent
-            close(pipefd[WRITE]);
-            char line[512];
-            ssize_t bytes = read(pipefd[READ], line, 512);
-            line[bytes] = '\0';
-            // Remove pwd from path
-            char* pwd = getenv("PWD");
+        char addr2line_cmd[512];
+        sprintf(addr2line_cmd, "addr2line -p -f -e %s %p", prg_name, buffer[i] - 1);
+        char line[512];
+        FILE* addr2line = popen(addr2line_cmd, "r");
+        fgets(line, 512, addr2line);
+        pclose(addr2line);
+        // Remove pwd from path
+        char* pwd = getenv("PWD");
+        char* pwd_pos = strstr(line, pwd);
+        if (pwd_pos != NULL) {
+            // Preserve text before pwd
+            char* line_pos = line;
             char* pwd_pos = strstr(line, pwd);
-            if (pwd_pos != NULL) {
-                // Preserve text before pwd
-                char* line_pos = line;
-                char* pwd_pos = strstr(line, pwd);
-                while (line_pos != pwd_pos) {
-                    printf("%c", *line_pos);
-                    line_pos++;
-                }
-                // Skip pwd
-                line_pos += strlen(pwd);
-                // Skip slash
+            while (line_pos != pwd_pos) {
+                printf("%c", *line_pos);
                 line_pos++;
-                // Print rest of line
-                printf("%s", line_pos);
             }
-            close(pipefd[READ]);
-            // Stop at main
-            if (strstr(line, "main at") != NULL) {
-                break;
-            }
+            // Skip pwd
+            line_pos += strlen(pwd);
+            // Skip slash
+            line_pos++;
+            // Print rest of line
+            printf("%s", line_pos);
         }
-
+        // Stop at main
+        if (strstr(line, "main at") != NULL) {
+            break;
+        }
     }
 }
 
