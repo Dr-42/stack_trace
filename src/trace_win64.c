62684f072c44ae4b09d934bcf45acb8d9cbf0da0
Spandan <royspandan1@gmail.com>
2023-11-13 22:04:10 +0530

    Call address instead of definition in windows

--- src/trace_win64.c
+++ src/trace_win64.c
@@ -15,34 +15,35 @@ void print_trace(char* prg_name){
 
   SymInitialize( process, NULL, TRUE );
 
-  frames               = CaptureStackBackTrace( 0, 100, stack, NULL );
+  frames               = CaptureStackBackTrace( 1, 100, stack, NULL ); // Start from index 1 to skip the current function (print_trace)
   symbol               = ( SYMBOL_INFO * )calloc( sizeof( SYMBOL_INFO ) + 256 * sizeof( char ), 1 );
   symbol->MaxNameLen   = 255;
   symbol->SizeOfStruct = sizeof( SYMBOL_INFO );
 
+  IMAGEHLP_LINE64 line;
+  line.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
+
   for( i = 0; i < frames; i++ )
   {
-    SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
-    // Get filename and line number from symbol->Address
-    char filename[1024];
+    SymFromAddr( process, ( DWORD64 )( stack[ i ]  - 1), 0, symbol );
+    // Get filename and line number from the address of the function call
     DWORD displacement;
-    IMAGEHLP_LINE64 line;
-    line.SizeOfStruct = sizeof(IMAGEHLP_LINE64);
-    if (SymGetLineFromAddr64(process, symbol->Address, &displacement, &line)) {
-      strcpy(filename, line.FileName);
+    if (SymGetLineFromAddr64(process, (DWORD64)(stack[i] - 1), &displacement, &line)) {
+      char filename[1024];
+      // Remove cwd from filename
+      GetCurrentDirectory(1024, filename);
+      char* cwd_ptr = strstr(line.FileName, filename);
+      if (cwd_ptr != NULL) {
+        strcpy(filename, cwd_ptr + strlen(filename) + 1);
+      }
+      // Print the stack trace
+      printf( "%s at %s:%lu\n", symbol->Name, filename, line.LineNumber );
     } else {
-      strcpy(filename, "unknown");
+      // Unable to get source file information
+      printf( "%s at (source information not available)\n", symbol->Name);
     }
-    // Remove cwd from filename
-    char cwd[1024];
-    GetCurrentDirectory(1024, cwd);
-    char* cwd_ptr = strstr(filename, cwd);
-    if (cwd_ptr != NULL) {
-      strcpy(filename, cwd_ptr + strlen(cwd) + 1);
-    }
-    // Print the stack trace
-    printf( "%s in %s:%lu\n", symbol->Name, filename, line.LineNumber );
     if (strcmp(symbol->Name, "main") == 0) {
+      // Stop printing the stack trace after reaching main
       break;
     }
   }
